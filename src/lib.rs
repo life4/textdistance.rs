f3fe5f72f2a38c11eb6d7f1731b376d762ec830a
gram <git@orsinium.dev>
2023-04-13 11:15:40 +0200

    lcsseq

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod textdistance {
     mod hamming;
+    mod lcsseq;
     mod smith_waterman;
 
     pub use self::hamming::hamming;
+    pub use self::lcsseq::lcsseq;
     pub use self::smith_waterman::smith_waterman;
 }
