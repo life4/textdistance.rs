20fc6f080d1bb6074d158d24ca1aacebe5600006
gram <git@orsinium.dev>
2023-04-16 14:10:22 +0200

    smoke property tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,28 @@ pub mod textdistance {
     pub use self::lcsstr::{lcsstr, LCSStr};
     pub use self::ratcliff_obershelp::{ratcliff_obershelp, RatcliffObershelp};
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::textdistance::Algorithm;
+
+    fn get_algs() -> Vec<Box<dyn Algorithm>> {
+        vec![
+            Box::new(textdistance::Hamming {}),
+            Box::new(textdistance::LCSSeq {}),
+            Box::new(textdistance::LCSStr {}),
+            Box::new(textdistance::RatcliffObershelp {}),
+        ]
+    }
+
+    #[test]
+    fn basic() {
+        for alg in get_algs() {
+            assert_eq!(alg.distance("", ""), 0);
+            assert!(alg.distance("ab", "cde") > 0);
+            // assert_eq!(alg.normalized_distance("", ""), 0.);
+            // assert_eq!(alg.normalized_similarity("", ""), 1.);
+        }
+    }
+}
