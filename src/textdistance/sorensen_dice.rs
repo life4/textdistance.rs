72aa8dae6909c47c2ea7342fc0cb90fdfaa38b41
gram <git@orsinium.dev>
2023-04-28 10:29:42 +0200

    more sorensen-dice tests

--- src/textdistance/sorensen_dice.rs
+++ src/textdistance/sorensen_dice.rs
@@ -31,7 +31,7 @@ impl Algorithm<f64> for SorensenDice {
 
 #[cfg(test)]
 mod tests {
-    use crate::textdistance::str::sorensen_dice;
+    use crate::textdistance::{str::sorensen_dice, Algorithm, SorensenDice};
     use assert2::assert;
     use rstest::rstest;
 
@@ -47,6 +47,46 @@ mod tests {
     #[case("test", "text", 2.0 * 3. / 8.)]
     fn function_str(#[case] s1: &str, #[case] s2: &str, #[case] exp: f64) {
         let act = sorensen_dice(s1, s2);
+        let ok = is_close(act, exp);
+        assert!(ok, "sorensen_dice({}, {}) is {}, not {}", s1, s2, act, exp);
+    }
+
+    #[rstest]
+    // parity with strsim
+    #[case("a", "a", 1.0)]
+    #[case("", "", 1.0)]
+    #[case("apple event", "apple event", 1.0)]
+    #[case("iphone", "iphone x", 0.9090909090909091)]
+    #[case("french", "quebec", 0.0)]
+    #[case("france", "france", 1.0)]
+    #[case("fRaNce", "france", 0.2)]
+    #[case("healed", "sealed", 0.8)]
+    #[case("web applications", "applications of the web", 0.7878787878)]
+    #[case("this has one extra word", "this has one word", 0.7741935483870968)]
+    #[case(
+        "this will have a typo somewhere",
+        "this will huve a typo somewhere",
+        0.92
+    )]
+    #[case(
+        "Olive-green table for sale, in extremely good condition.",
+        "For sale: table in very good  condition, olive green in colour.",
+        0.6060606060606061
+    )]
+    #[case(
+        "Olive-green table for sale, in extremely good condition.",
+        "For sale: green Subaru Impreza, 210,000 miles",
+        0.2558139534883721
+    )]
+    #[case(
+        "Olive-green table for sale, in extremely good condition.",
+        "Wanted: mountain bike with at least 21 gears.",
+        0.1411764705882353
+    )]
+    fn for_bigrams(#[case] s1: &str, #[case] s2: &str, #[case] exp: f64) {
+        let s1 = &s1.replace(' ', "");
+        let s2 = &s2.replace(' ', "");
+        let act = SorensenDice::default().for_bigrams(s1, s2).nval();
         let ok = is_close(act, exp);
         assert!(ok, "sorensen_dice({}, {}) is {}, not {}", s1, s2, act, exp);
     }
